2390bd86ffd8a001f13e955649e285fd02608d93
AnkushAggarwalll <aggarwalankush28@gmail.com>
2021-04-12 17:42:08 +0530

    abc

---
+++ data.js
@@ -0,0 +1,28 @@
+var data={
+    key:String,
+    age:Number,
+    description:String
+}
+
+
+function submit(){
+    var key= document.getElementById("key").value;
+    var val= document.getElementById("val").value;
+    var desc= document.getElementById("desc").value;
+    if(localStorage.getItem(key))
+    alert('key already exists');
+    else if(key=="" || val=="" || desc=="")
+    alert("Enter the below fields");
+else{
+    data.key=key;
+    data.age=val;
+    data.description=desc;
+localStorage.setItem(key,JSON.stringify(data));
+    }
+    document.getElementById("key").value="";
+    document.getElementById("val").value="";
+    document.getElementById("desc").value="";
+}
+function keyRoute() {
+    window.location = '/read';
+}
